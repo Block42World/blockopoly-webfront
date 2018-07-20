2f4fc618049cf91ec6037e21ebe6bb4bb5186af2
t-PC\Jim <dfjhde@gmail.com>
2018-07-20 16:47:59 +1000

    new model

---
+++ js/block42/Editor.js
@@ -0,0 +1,21 @@
+class Editor
+{
+	static Save()
+	{
+		for (var i = world.blocks.length - 1; i >= 0; i--) {
+			if(){}
+	    	world.blocks[voxelData.voxels[i].x][voxelData.voxels[i].y][voxelData.voxels[i].z] =// blank
+			{
+				id: 19,
+				spawnable: true,
+				transparent: false,
+				selflit: true,
+				gravity: false,
+				fluid: false,
+				isColorful: true,
+				colorID: voxelData.voxels[i].colorIndex,
+				texture: function( world, lightmap, lit, x, y, z, dir ) { return [ 2.1/16, 4.1/16, 2.9/16, 4.9/16 ]; }
+			};
+		}
+	}
+}
